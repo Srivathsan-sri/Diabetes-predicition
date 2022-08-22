aa53d34dc9a7941c5b2f1374c23eb67460e10707
Vathsan <saisri2506@gmail.com>
2022-08-22 22:14:08 +0530

    first commit

---
+++ prediction/views.py
@@ -0,0 +1,55 @@
+from unittest import result
+from webbrowser import get
+from django.shortcuts import render
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn import svm
+from sklearn.metrics import accuracy_score
+# Create your views here.
+def predict(request):
+    return render(request,'predict.html')
+
+def results(request):
+    diabetes_dataset = pd.read_csv(r'static\diabetes.csv') 
+    X = diabetes_dataset.drop(columns = 'Outcome', axis=1)
+    Y = diabetes_dataset['Outcome']
+    scaler = StandardScaler()
+    scaler.fit(X)
+    standardized_data = scaler.transform(X)
+    X = standardized_data
+    Y = diabetes_dataset['Outcome']
+    X_train, X_test, Y_train, Y_test = train_test_split(X,Y, test_size = 0.2, stratify=Y, random_state=2)
+    classifier = svm.SVC(kernel='linear')
+    classifier.fit(X_train, Y_train)
+    val1=float(request.GET['n1'])
+    val2=float(request.GET['n2'])
+    val3=float(request.GET['n3'])
+    val4=float(request.GET['n4'])
+    val5=float(request.GET['n5'])
+    val6=float(request.GET['n6'])
+    val7=float(request.GET['n7'])
+    val8=float(request.GET['n8'])
+    input_data = ([[val1,val2,val3,val4,val5,val6,val7,val8]])
+
+    # changing the input_data to numpy array
+    input_data_as_numpy_array = np.asarray(input_data)
+
+    # reshape the array as we are predicting for one instance
+    input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+
+    # standardize the input data
+    std_data = scaler.transform(input_data_reshaped)
+
+    prediction = classifier.predict(std_data)
+    
+    
+          
+    result=''
+    if (prediction[0] == 0):
+       result='Negative'
+    else:
+      result='positive'
+
+    return render(request,'predict.html',{'result':result})
